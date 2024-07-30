d8e059d66c5aef474bc995df5030c7f34a7b5ccd
Hossam sulleman <sullemanhossam4@gmail.com>
2024-07-30 03:20:37 +0100

    sucess

--- cli/battleship-corbitae/source/app.js
+++ cli/battleship-corbitae/source/app.js
@@ -1,8 +1,26 @@
+import React, {useEffect, useState} from 'react';
 import {Box, Text} from 'ink';
-import React from 'react';
 import Gradient from 'ink-gradient';
 import BigText from 'ink-big-text';
-// import packageJson from '../package.json' with {type: 'json'};
+import fs from 'fs/promises';
+import path from 'path';
+
+// Function to read and parse package.json
+const readPackageJson = async () => {
+	// Define the path to the package.json file
+	const packageJsonPath = path.join(process.cwd(), 'package.json');
+
+	try {
+		// Read the package.json file
+		const data = await fs.readFile(packageJsonPath, 'utf8');
+		// Parse the JSON data
+		const packageJson = JSON.parse(data);
+		return packageJson;
+	} catch (error) {
+		console.error('Error reading or parsing package.json:', error);
+		return null;
+	}
+};
 
 // Define task-related functions
 const generateIndexTask = name => (
@@ -11,16 +29,32 @@ const generateIndexTask = name => (
 	</Text>
 );
 
+const restoreArchivesTask = name => (
+	<Text>
+		Restoring archives for <Text color="green">{name}</Text>
+	</Text>
+);
+
 // Define task options
-const options = {
+const workspace_options = {
+	template: {
+		'restore-archives': restoreArchivesTask,
+	},
+};
+
+const project_options = {
 	javascript: {
 		'generate-index': generateIndexTask,
 	},
 };
 
 // Functional Router component
-const Router = ({name = 'Stranger', task, type}) => {
-	const typeOptions = options[type];
+const Router = ({name = 'Stranger', task, type, pkg}) => {
+	// Determine the correct options based on the presence of pkg.workspaces
+	const typeOptions = pkg?.workspaces
+		? workspace_options[type]
+		: project_options[type];
+
 	if (!typeOptions) {
 		return <Text color="red">Error: Unknown type "{type}"</Text>;
 	}
@@ -38,18 +72,37 @@ const Router = ({name = 'Stranger', task, type}) => {
 };
 
 // Functional Title component
-const Title = () => (
-	<Gradient name="mind">
-		<BigText text={'hi'} />
-	</Gradient>
-);
+const Title = ({pkg}) => {
+	if (!pkg) {
+		return <Text color="red">Error: Could not load package.json</Text>;
+	}
+
+	return (
+		<Gradient name="mind">
+			<BigText text={pkg.name} />
+		</Gradient>
+	);
+};
 
 // Functional App component
-const App = props => (
-	<Box flexDirection="column">
-		<Title />
-		<Router {...props} />
-	</Box>
-);
+const App = props => {
+	const [pkg, setPkg] = useState(null);
+
+	useEffect(() => {
+		const fetchPackageJson = async () => {
+			const packageJson = await readPackageJson();
+			setPkg(packageJson);
+		};
+
+		fetchPackageJson();
+	}, []);
+
+	return (
+		<Box flexDirection="column">
+			<Title pkg={pkg} />
+			<Router pkg={pkg} {...props} />
+		</Box>
+	);
+};
 
 export default App;
