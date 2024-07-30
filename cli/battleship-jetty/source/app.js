16c82bbd82fbac2d5a7b783b125e5127f5a7545a
Hossam sulleman <sullemanhossam4@gmail.com>
2024-07-30 04:05:50 +0100

    push

--- cli/battleship-jetty/source/app.js
+++ cli/battleship-jetty/source/app.js
@@ -5,23 +5,46 @@ import {render, Text, Box} from 'ink';
 
 export default function App() {
 	const [output, setOutput] = React.useState('');
+	const [error, setError] = React.useState('');
 
 	React.useEffect(() => {
-		const subProcess = childProcess.spawn('ls', []);
+		const subProcess = childProcess.spawn('npx', ['lerna', 'init']);
 
-		// eslint-disable-next-line @typescript-eslint/ban-types
+		// Handle stdout
 		subProcess.stdout.on('data', newOutput => {
 			const lines = stripAnsi(newOutput.toString('utf8')).split('\n');
-			setOutput(lines.slice(-5).join('\n'));
+			setOutput(prevOutput => `${prevOutput}\n${lines.slice(-5).join('\n')}`);
 		});
-	}, [setOutput]);
+
+		// Handle stderr
+		subProcess.stderr.on('data', errOutput => {
+			setError(prevError => `${prevError}\n${stripAnsi(errOutput.toString('utf8'))}`);
+		});
+
+		// Handle errors
+		subProcess.on('error', err => {
+			setError(prevError => `${prevError}\nError: ${err.message}`);
+		});
+
+		// Cleanup on unmount
+		return () => {
+			subProcess.kill();
+		};
+	}, []);
 
 	return (
 		<Box flexDirection="column" padding={1}>
-			<Text>Сommand output:</Text>
+			<Text>Command output:</Text>
 			<Box marginTop={1}>
 				<Text>{output}</Text>
 			</Box>
+			{error && (
+				<Box marginTop={1}>
+					<Text color="red">Error:</Text>
+					<Text color="red">{error}</Text>
+				</Box>
+			)}
 		</Box>
 	);
 }
+   
